739bd907d2fb64b8b1a66c8e94b1af050f56ddd0
MegaLeap <zhang_jian_flow@hotmail.com>
2025-04-20 20:56:55 +0800

    Add handling for receipt items and implement printing
    Introduced a list to track items in the Receipt class and modified the `print` method to iterate over the items, using their `print` method. Implemented the Item class constructor and updated the `add` method to populate the items list accordingly.

--- src/main/java/self/jjjyjj/chapter4_collaboration/Item.java
+++ src/main/java/self/jjjyjj/chapter4_collaboration/Item.java
@@ -4,6 +4,14 @@ package self.jjjyjj.chapter4_collaboration;
  * A simple item class that can be printed.
  */
 public class Item {
+    private final String description;
+    private final Money price;
+
+    public Item(String description, Money price) {
+        this.description = description;
+        this.price = price;
+    }
+
     public void print(Printer p) {
         // TODO
     }
--- src/main/java/self/jjjyjj/chapter4_collaboration/Receipt.java
+++ src/main/java/self/jjjyjj/chapter4_collaboration/Receipt.java
@@ -1,5 +1,8 @@
 package self.jjjyjj.chapter4_collaboration;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Class that able to add some items and print them all.
  * The same printer is used to print out the entire receipt.
@@ -7,17 +10,21 @@ package self.jjjyjj.chapter4_collaboration;
  */
 public class Receipt {
     private final Printer printer;
+    private final List<Item> items = new ArrayList<>();
 
     public Receipt(Printer printer) {
         this.printer = printer;
     }
 
+    /**
+     * This method is an example of applying the "Call don't ask".
+     */
     public void print() {
-        // TODO - using field printer to print out the receipt
+        items.forEach(item -> item.print(printer));
     }
 
     public void add(String description, Money price) {
-        // TODO
+        items.add(new Item(description, price));
     }
 
 
